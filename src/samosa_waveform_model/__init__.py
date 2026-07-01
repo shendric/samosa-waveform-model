5770b13064ed37849afdaacf4197b38991246a20
Stefan Hendricks <stefan.hendricks@awi.de>
2026-07-01 09:32:03 +0200

    Check for cython imports

--- src/samosa_waveform_model/__init__.py
+++ src/samosa_waveform_model/__init__.py
@@ -6,13 +6,17 @@ python package for the SAMOSA+  conversion. Based on sampy by CLS
 
 __author__ = "Stefan Hendricks <stefan.hendricks@awi.de>"
 
+import warnings
 from samosa_waveform_model.dataclasses import SensorParameters, SARParameters, PlatformLocation, WaveformModelParameters
 from samosa_waveform_model.model import ScenarioData, SAMOSAWaveformModel
 from samosa_waveform_model.lut import CS2_LOOKUP_TABLES
 
+from samosa_waveform_model.model_cy import SAMOSAWaveformModel as SAMOSAWaveformModelCython
+
 try:
     from samosa_waveform_model.model_cy import SAMOSAWaveformModel as SAMOSAWaveformModelCython
-except ImportError:
+except ImportError as ie:
+    warnings.warn(f"Could not import SAMOSAWaveformModelCython: {ie}")
     SAMOSAWaveformModelCython = None
 
 __all__ = [
